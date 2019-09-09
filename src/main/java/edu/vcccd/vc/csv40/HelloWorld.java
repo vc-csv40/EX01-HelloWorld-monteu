d95c19bdfa7fa66467dfe8cd5cad7aa840005d54
monteu <k.hofteig@gmail.com>
2019-09-09 16:38:19 -0700

    Committed!

---
+++ src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
@@ -0,0 +1,18 @@
+package edu.vcccd.vc.csv40;
+
+/**
+ * CS V40 Beginning Java
+ * Assignment: edu.vcccd.vc.csv40.HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author Kody Hofteig
+ */
+
+public class HelloWorld {
+
+public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        }
+        }
